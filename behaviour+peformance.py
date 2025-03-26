2a755d9c42731f4503ce607c9fe6aa7b6a9e8ce1
Olivia Parris <olivia.parris@btinternet.com>
2025-03-26 11:16:28 +0000

    graph

--- behaviour+peformance.py
+++ behaviour+peformance.py
@@ -2,18 +2,11 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#with open('Students_Grading_Dataset.csv' , 'r') as f:
-  #  csv_reader = csv.reader(f)
-   # header_row = next(csv_reader)
-    #print(header_row)
+
 
 data = pd.read_csv("Students_Grading_Dataset.csv")
 print(data)
-#for i in range(len(header_row)):
- #   print(i,':', header_row[i])
 
-#data = data.dropna(subset=['Study_Hours_per_Week', 'Stress_Level (1-10)'])
-# Round specific columns to whole numbers
 data['Study_Hours_per_Week'] = data['Study_Hours_per_Week'].round()
 
 
@@ -31,13 +24,8 @@ plt.title('title')
 plt.grid(True, axis='x', linestyle='--', alpha=0.7)
 
 plt.xlim(min(x) - 1, max(x) + 1)
-#x = [4, 6, 8, 10, 12, 14, 16, 18, 20]
-#y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 
 
 plt.legend()
 plt.grid(True, axis= 'y', linestyle='--', alpha= 0.7)
-
-
 plt.show()
