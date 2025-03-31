5dce7db3b423ac1f2bb325654552432e41becb09
Olivia Parris <olivia.parris@btinternet.com>
2025-03-31 17:04:23 +0100

    none

--- max_visualisations.py
+++ max_visualisations.py
@@ -5,7 +5,7 @@ from data_loader import load_data
 def max_visulisation_one():
     # load data:
     data = load_data()
-    print(data)
-    # print(data.head())
+    # print(data)
+    print(data.head())
 
 max_visulisation_one()
