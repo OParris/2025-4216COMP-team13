b1c4ea8b74a351a42f85f9ab640595aab072e6d1
Olivia Parris <olivia.parris@btinternet.com>
2025-03-31 16:54:58 +0100

    Update plots for graphs

--- Q2_Louisa.py
+++ Q2_Louisa.py
@@ -4,17 +4,21 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("Students_Grading_Dataset.csv")
 
-data['Final_Score'] = data['Final_Score'].round()
-data['Internet_Access_at_Home'] = data['Internet_Access_at_Home']
 df = pd.DataFrame(data)
+
+data['Final_Score'] = data['Final_Score'].mean()
+data['Internet_Access_at_Home'] = data['Internet_Access_at_Home']
+
+Mean_Final_Score = df.groupby('Internet_Access_at_Home')['Final_Score'].mean().reset_index()
 
 fig, ax = plt.subplots()
 
-plt.bar(data['Final_Score'], data['Internet_Access_at_Home'])
+plt.bar(Mean_Final_Score['Internet_Access_at_Home'], Mean_Final_Score['Final_Score'])
 
 plt.xlabel('Final Score')
 plt.ylabel('Internet Access from home')
 plt.title('Students that have internet access from home.')
+ax.set_yticks(range(0, 100, 10))
 
 plt.legend()
 plt.grid(True, axis= 'y', linestyle='--', alpha= 0.7)
