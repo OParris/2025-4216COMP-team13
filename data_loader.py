f1dd45903f7b9c1ad0daeffb4fb9f2bb3f2be24d
Max Cowen-Carr <mcarr@Maxs-MacBook-Pro.local>
2025-04-02 00:13:09 +0100

    finished first visualisation for first question

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,7 @@ def load_data():
     data = pd.read_csv('Students_Grading_Dataset.csv')
 
     # data['Study_Hours_per_Week'] = data['Study_Hours_per_Week'].round()
-    print(data)
+    # print(data)
     return data
 
 
