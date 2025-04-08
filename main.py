32526de0fd4e9818a4a62d4927145698c34791b4
JakeBailey01 <CSMJBAIL@ljmu.ac.uk>
2025-04-08 12:14:27 +0100

    Updated visualisations and added functions into main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
-from Q1_Max import max_visualisation_one  
+from Q1_Max import max_visualisation_one
+from Jake_questions import jake_visualisation_one
+from Jake_questions import jake_visualisation_two
+from Jake_questions import jake_visualisation_three
 import Q1_Max
 # from data_loader import load_data
 
@@ -14,5 +17,20 @@ root.title("Team 13 Visualisations")
 
 btn_max_vis = tk.Button(root, text="Max Visualisation-One", command=run_max_visualisation)
 btn_max_vis.pack(pady=10)
+
+def run_jake_visualisation_one():
+    jake_visualisation_one
+btn_jake_vis = tk.Button(root,text="Jake Visualisation-One", command=jake_visualisation_one)
+btn_jake_vis.pack(pady=10)
+
+def run_jake_visualisation_two():
+    jake_visualisation_two
+btn_jake_vis = tk.Button(root,text="Jake Visualisation-Two", command=jake_visualisation_two)
+btn_jake_vis.pack(pady=10)
+
+def run_jake_visualisation_three():
+    jake_visualisation_three
+btn_jake_vis = tk.Button(root,text="Jake Visualisation-Three", command=jake_visualisation_three)
+btn_jake_vis.pack(pady=10)
 
 root.mainloop()
