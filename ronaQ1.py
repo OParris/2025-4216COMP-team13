22ba3d819a681d106a3f53bc23732bdf7dc0161e
JakeBailey01 <CSMJBAIL@ljmu.ac.uk>
2025-04-08 17:10:12 +0100

    Added functions to Olivias and Ronas visualisations, minor bug fixes such as incorrect data being processed in the visualisation. Added data_load to improve efficiency

--- ronaQ1.py
+++ ronaQ1.py
@@ -1,23 +1,28 @@
 import csv 
 import  pandas as pd 
 import matplotlib.pyplot as plt 
+from data_loader import load_data
 
-data = pd.read_csv("Students_Grading_Dataset.csv")
+def Rona_Visualisation_One():
+    data=load_data()
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(data)
+    data['Fmaily_Income_Level'] = data['Family_Income_Level']
+    data['Attendance (%)'] = data['Attendance (%)'].mean()
 
-data['Fmaily_Income_Level'] = data['Family_Income_Level']
-data['Attendance (%)'] = data['Attendance (%)']
+    #== df.groupby('Attendance (%)')['Family_Income_Level'].mean().reset_index()
 
-#== df.groupby('Attendance (%)')['Famil_Income_Level'].mean().reset_index()
+    fig,ax = plt.subplots() 
 
-fig,ax = plt.subplots() 
+    plt.bar(data['Family_Income_Level'],data['Attendance (%)'])
 
-plt.bar(data['Family_Income_Level'],data['Attendance (%)'])
+    plt.xlabel('Family income level')
+    plt.ylabel('Attendance (%)')
+    plt.title('How Family Income Has An Impact On Attendance')
+    plt.ylim(0,100)
 
-plt.xlabel('Fmily_Income_Level')
-plt.ylabel('Attendance (%)')
-plt.title('How Family Income Has An Impact On Attendance')
 
-plt.legend()
-plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    Rona_Visualisation_One()
