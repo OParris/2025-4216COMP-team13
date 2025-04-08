c24cb7a576f15f38a6880afb0807dde5e395b4cb
zahra14a <zahraanwar1406@gmail.com>
2025-04-08 11:12:41 +0100

    Create Q1_Zahra.py

---
+++ Q1_Zahra.py
@@ -0,0 +1,23 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("Students_Grading_Dataset.csv")
+print(data)
+
+data['Study_Hours_per_Week'] = data['Final_Score'].round()
+df = pd.DataFrame(data)
+
+data['Study_Hours_per_Week'] = data['Study_Hours_per_Week']
+data['Final_Score'] = data['Final_Score']
+
+fig, ax = plt.subplots()
+plt.scatter(data['Study_Hours_per_Week'], data['Final_Score'], color='blue')
+
+plt.xlabel('Hours studied per week')
+plt.ylabel('Final Score')
+plt.title('Overall score for students with most hours studied per week.')
+
+plt.legend()
+plt.grid(True, axis= 'y', linestyle='--', alpha= 0.7)
+plt.show()
